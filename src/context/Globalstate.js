ce99eba41c8c7c1bf30cb4609650ccae4065b110
UnanamCharles <151572673+UnanamCharles@users.noreply.github.com>
2024-10-10 10:23:47 -0700

    final changes

--- src/context/Globalstate.js
+++ src/context/Globalstate.js
@@ -62,7 +62,7 @@ export const GlobalProvider = ({ children }) => {
     return (
         <GlobalContext.Provider value={{
             transactions: state.transactions,
-            deleteTransaction,
+            Transaction,
             addTransaction,
             dispatch
         }}>
